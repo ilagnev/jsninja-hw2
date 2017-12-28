1bbd28f7d7440e50ea08a9269d11487791bfc33c
skate <ilagnev@gmail.com>
2017-12-28 16:57:28 +0200

    add async test and run tests on precommit

--- test/ava.test.js
+++ test/ava.test.js
@@ -3,3 +3,8 @@ import test from 'ava';
 test('ava pass simple test', t => {
   t.pass();
 });
+
+test('ava pass async test', async t => {
+  const bar = Promise.resolve('bar');
+  t.is(await bar, 'bar');
+});
